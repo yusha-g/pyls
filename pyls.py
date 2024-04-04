497a606b43e053c1bff8f762270e35465fd60b63
yusha-g <pratyusha.gh@gmail.com>
2024-04-05 00:54:06 +0530

    feat: implement list items method

--- pyls.py
+++ pyls.py
@@ -8,7 +8,9 @@ from typing import Optional
 
 
 class Items:
-
+    """
+    If an Item has contents, it is a directory. Otherwise, it's a file.
+    """
     def __init__(
         self, 
         name: str, 
@@ -26,9 +28,19 @@ class Items:
         if contents:
             for sub_items in contents:
                 self.contents.append(Items(**sub_items))
+    
+    def list_items(self) -> None:
+        """
+        Lists all items in the immediate subdirectory.
+        """
+        for sub_items in self.contents:
+            if sub_items.name[0] != ".":
+                print(sub_items.name, end=" ")
+        print()
 
 if __name__ == "__main__":
     structure_path = Path(__file__).parent / "structure.json"
     with open(structure_path) as fh:
         file_structure = json.load(fh)
     item = Items(**file_structure)
+    item.list_items()
