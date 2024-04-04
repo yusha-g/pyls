7e5975d3b272a2db605540d007eb601237ae8d8b
yusha-g <pratyusha.gh@gmail.com>
2024-04-04 23:30:44 +0530

    feat: setup class Items

--- pyls.py
+++ pyls.py
@@ -1,4 +1,28 @@
 #!/usr/bin/env python3
 
+import json
+from pathlib import Path
+from re import I
+from typing import Optional
+from pydantic import BaseModel
+    
+class Items(BaseModel):
+    name: str
+    size: int
+    time_modified: int
+    permissions: str
+    contents: Optional[list["Items"]] = None
+
+    def list_items(self):
+        if self.contents is not None:
+            for items in self.contents:
+                print(items.name, end=" ")
+            print()
+
 if __name__ == "__main__":
-    ...
+    structure_path = Path(__file__).parent / "structure.json"
+    with open(structure_path) as fh:
+        file_structure = json.load(fh)
+
+    file = Items(**file_structure)
+    file.list_items()
