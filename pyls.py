df959aa027f4c2a85c623eb1c9fe8b7536d35a7f
yusha-g <pratyusha.gh@gmail.com>
2024-04-07 01:31:15 +0530

    refactor: create separate scripts for class item  and args

--- pyls.py
+++ pyls.py
@@ -1,48 +1,11 @@
 #!/usr/bin/env python3
 
 import argparse
-from datetime import datetime
 import json
 from pathlib import Path
-from typing import Optional
 
-def add_args(parser):
-    parser.add_argument(
-        "--all", "-A", 
-        help="List all directories", 
-        action="store_true"
-    )
-    parser.add_argument(
-        "--long", "-l",
-        help="List additional information",
-        action="store_true"
-    )
-    parser.add_argument(
-        "--reverse", "-r",
-        help="Reverse listing",
-        action="store_true"
-    )
-    parser.add_argument(
-        "--time","-t",
-        help="Sort by time-modified",
-        action="store_true"
-    )
-
-    parser.add_argument(
-        "--filter",
-        help="Filter by type of item. Either file or dir",
-        choices=['dir','file'],
-        action="store"
-    )
-
-    # if no file path is produced, use None
-    parser.add_argument(
-        "file_path",
-        help="The path to the directory or file to list",
-        type=str,
-        nargs="?",
-        default=None
-    )
+from item import Item
+from pyls_arguments import add_args
 
 def get_current_file_content(item, file_path):
     if item.name == file_path or file_path is None:
@@ -53,69 +16,6 @@ def get_current_file_content(item, file_path):
             return current_item
     return None
     
-
-class Item:
-    """
-    Each Item corresponds to directory/file in the structure.
-    If an Item has contents, it is a directory. Otherwise, it's a file.
-    """
-    def __init__(
-        self, 
-        name: str, 
-        size: int, 
-        time_modified: int, 
-        permissions: str, 
-        contents: Optional[list["Item"]] = None
-    ):
-
-        self.name = name
-        self.size = size
-        self.time_modified = datetime.fromtimestamp(time_modified).strftime("%b %d %H:%M")
-        self.permissions = permissions
-        self.contents = []
-        if contents:
-            for sub_items in contents:
-                self.contents.append(Item(**sub_items))
-    
-    def list_items(self, list_all: bool, filter=None) -> list:
-        """
-        Args:
-            list_all (bool): True if -A is set
-        Returns: 
-            List of all items in the immediate subdirectory.
-            If list_all is True, will include files starting with ".".
-        """
-        item_list = []
-        if list_all:
-            for sub_items in self.contents:
-                item_list.append(sub_items)
-            return item_list
-
-        for sub_items in self.contents:
-            if sub_items.name[0] != ".":
-                item_list.append(sub_items)
-        return item_list
-    
-    def sort_by_time(self, item_list: list) -> list:
-        return sorted(item_list, key=lambda item_time: item.time_modified)
-    
-    def reverse_items(self, item_list: list) -> list:
-        return item_list[::-1]
-
-    def display(self, item_list):
-        for item in item_list:
-            print(item.name, end=" ")
-        print()
-    
-    def long_list_items(self, item_list) -> None:
-        for item in item_list:
-            print(
-                item.permissions,
-                item.size,
-                item.time_modified,
-                item.name
-            )
-
 if __name__ == "__main__":
     structure_path = Path(__file__).parent / "structure.json"
     with open(structure_path) as fh:
@@ -123,13 +23,18 @@ if __name__ == "__main__":
     item = Item(**file_structure)
 
     parser = argparse.ArgumentParser()
-    
-    add_args(parser)
+    pyls_parser = parser.add_argument_group("pyls flags")
+    add_args(pyls_parser)
 
     args = parser.parse_args()
-
-    current_item = get_current_file_content(item, args.file_path)
-    item_list = current_item.list_items(args.all, filter=args.filter)
+    current_item = get_current_file_content(
+        item, 
+        args.file_path.split("/")[-1] if args.file_path is not None else None
+    )
+    if current_item:
+        item_list = current_item.list_items(args.all, filter=args.filter)
+    else:
+        raise NameError
 
 
     """Note: 
