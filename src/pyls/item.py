17e276ad5715dddf5d6da056a20693c3a36fc34a
yusha-g <pratyusha.gh@gmail.com>
2024-04-07 17:19:55 +0530

    refactor: format with black

--- src/pyls/item.py
+++ src/pyls/item.py
@@ -1,40 +1,43 @@
-
 from datetime import datetime
 from typing import Optional
+
 
 class Item:
     """
     Each Item corresponds to directory/file in the structure.
     If an Item has contents, it is a directory. Otherwise, it's a file.
     """
+
     def __init__(
-        self, 
-        name: str, 
-        size: int, 
-        time_modified: int, 
-        permissions: str, 
-        contents: Optional[list["Item"]] = None
+        self,
+        name: str,
+        size: int,
+        time_modified: int,
+        permissions: str,
+        contents: Optional[list["Item"]] = None,
     ):
 
         self.name = name
         self.size = size
-        self.time_modified = datetime.fromtimestamp(time_modified).strftime("%b %d %H:%M")
+        self.time_modified = datetime.fromtimestamp(time_modified).strftime(
+            "%b %d %H:%M"
+        )
         self.permissions = permissions
         self.contents = []
         if contents:
             for sub_items in contents:
                 self.contents.append(Item(**sub_items))
-    
+
     def __repr__(self) -> str:
         # 'cause I actually want to understand what I'm looking
         return f"{self.name}"
-    
+
     def list_items(self, list_all: bool, filter: Optional[str] = None) -> list:
         """
         Args:
             list_all (bool): True if -A is set
             filter (str): Specifies type of item to list with options dir or file.
-        Returns: 
+        Returns:
             List of all items in the immediate subdirectory.
             If list_all is True, will include files starting with ".".
         """
@@ -44,26 +47,21 @@ class Item:
             if list_all or sub_item.name[0] != "."
         ]
         filtered_list = item_list
-        if filter=="dir":
+        if filter == "dir":
             filtered_list = [
-                filtered_item
-                for filtered_item in item_list
-                if filtered_item.contents
+                filtered_item for filtered_item in item_list if filtered_item.contents
             ]
             return filtered_list
 
         filtered_list = [
-            filtered_item
-            for filtered_item in item_list
-            if not filtered_item.contents
+            filtered_item for filtered_item in item_list if not filtered_item.contents
         ]
         return filtered_list
-        
-    
+
     def sort_by_time(self, item_list: list) -> list:
         """Sorts item list by time_modified"""
         return sorted(item_list, key=lambda item_time: item_time.time_modified)
-    
+
     def reverse_items(self, item_list: list) -> list:
         """Reverses listing of items"""
         return item_list[::-1]
@@ -73,13 +71,8 @@ class Item:
         for item in item_list:
             print(item.name, end=" ")
         print()
-    
+
     def long_list_items(self, item_list: list) -> None:
         """Display function for long listing"""
         for item in item_list:
-            print(
-                item.permissions,
-                item.size,
-                item.time_modified,
-                item.name
-            )
+            print(item.permissions, item.size, item.time_modified, item.name)
--- src/pyls/pyls.py
+++ src/pyls/pyls.py
@@ -9,14 +9,17 @@ from custom_exceptions import InvalidPath
 from item import Item
 from pyls_arguments import add_args
 
-def get_current_file_content(item: Item, file_path: Optional[str] = None) -> Item|None:
+
+def get_current_file_content(
+    item: Item, file_path: Optional[str] = None
+) -> Item | None:
     """
     Args:
         item (Item): Structure containing file.
         file_path (str): Name of file to be accessed.
     Return:
-        Contents of item specified in file path. 
-        If no item matches, None is returned. 
+        Contents of item specified in file path.
+        If no item matches, None is returned.
     """
     if item.name == file_path or file_path is None:
         return item
@@ -25,8 +28,9 @@ def get_current_file_content(item: Item, file_path: Optional[str] = None) -> Ite
         if current_item is not None:
             return current_item
     return None
-    
-def main():
+
+
+if __name__ == "__main__":
     structure_path = Path(__file__).parent / "json" / "structure.json"
     with open(structure_path) as fh:
         file_structure = json.load(fh)
@@ -40,16 +44,12 @@ def main():
 
     # extract file name if file_path is specified, else just assign it None
     file_name = args.file_path.split("/")[-1] if args.file_path is not None else None
-    current_item = get_current_file_content(
-        item, 
-        file_name
-    )
+    current_item = get_current_file_content(item, file_name)
     if current_item:
         item_list = current_item.list_items(args.all, filter=args.filter)
     else:
         assert file_name is not None
         raise InvalidPath(file_name)
-
 
     """Note: 
     Order of checking the args is important here.
@@ -59,12 +59,10 @@ def main():
     """
 
     if args.time:
-        item_list=item.sort_by_time(item_list)
+        item_list = item.sort_by_time(item_list)
     if args.reverse:
-        item_list=item.reverse_items(item_list)
+        item_list = item.reverse_items(item_list)
     if args.long:
         item.long_list_items(item_list)
     else:
         item.display(item_list)
-    
-
