83ec8c9002737b24a3e6f09d6bc7ea90e0a38d33
yusha-g <pratyusha.gh@gmail.com>
2024-04-11 19:32:44 +0530

    feat: add relative path functionality

--- src/pyls/pyls.py
+++ src/pyls/pyls.py
@@ -10,7 +10,7 @@ from item import Item
 from pyls_arguments import add_args
 
 
-def get_current_file_content(
+def get_current_file_content_old(
     item: Item, file_path: Optional[str] = None
 ) -> Item | None:
     """
@@ -24,11 +24,36 @@ def get_current_file_content(
     if item.name == file_path or file_path is None:
         return item
     for sub_item in item.contents:
-        current_item = get_current_file_content(sub_item, file_path)
+        current_item = get_current_file_content_old(sub_item, file_path)
         if current_item is not None:
             return current_item
     return None
 
+# TODO: simplify the below code
+def get_current_file_content(
+    item: Item, file_path: Optional[list] = None
+):
+    if file_path is None:
+        return item
+    if file_path[0] == "." and len(file_path) == 1:
+        return item
+    if file_path[0] == "." and len(file_path)>1:
+        file_path[0] = "interpreter"
+
+    if file_path[0] == item.name:
+        if len(file_path) == 1:
+            return item
+        else:
+            for sub_item in item.contents:
+                current_item = get_current_file_content(sub_item, file_path[1:])
+                if current_item is not None:
+                    return current_item
+    for sub_item in item.contents:
+            current_item = get_current_file_content(sub_item, file_path)
+            if current_item is not None:
+                    return current_item
+    return None
+    
 
 if __name__ == "__main__":
     structure_path = Path(__file__).parent / "json" / "structure.json"
@@ -46,11 +71,9 @@ if __name__ == "__main__":
     # extract file name if file_path is specified, else just assign it None
     file_name = None
     if args.file_path:
-        file_name = args.file_path.split("/")[-1]
-        if file_name == ".":
-            file_name = None
+        file_name = args.file_path.split("/")
     
-
+    # breakpoint()
     current_item = get_current_file_content(item, file_name)
     if current_item:
         item_list = current_item.list_items(args.all, filter=args.filter)
